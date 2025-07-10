d05a39a55db4485ce39aefa64cd3ea7c7485b282
eric@statsig.com <111610731+ealui-statsig@users.noreply.github.com>
2025-07-09 19:29:08 -0700

    Update peekable.rs

--- core/lib/src/data/peekable.rs
+++ core/lib/src/data/peekable.rs
@@ -36,10 +36,7 @@ impl<const N: usize, R: AsyncRead + Unpin> Peekable<N, R> {
                     break;
                 },
                 Ok(_) => { /* continue */ },
-                Err(e) => {
-                    error_!("Failed to read into peek buffer: {:?}.", e);
-                    break;
-                }
+                Err(_e) => { /* continue */ },
             }
         }
 
